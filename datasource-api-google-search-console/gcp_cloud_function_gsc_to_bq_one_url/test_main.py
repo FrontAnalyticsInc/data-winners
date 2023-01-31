fa2757f131963586a964a8dffec9f410637841ba
alton <alton@frontanalytics.com>
2023-01-31 04:22:55 -0700

    gsc cf update

---
+++ datasource-api-google-search-console/gcp_cloud_function_gsc_to_bq_one_url/test_main.py
@@ -0,0 +1,31 @@
+
+from main import run
+import datetime
+
+
+def test_main():
+    '''Test main locally'''
+
+
+    # construct input variables
+    site = "bikelanes.com"
+    page_url = "https://bikelanes.com/"
+    today = datetime.datetime.today()
+    start_date = today - datetime.timedelta(days=4)
+    end_date = today - datetime.timedelta(days=3)
+    BQ_DATASET_NAME = 'data_winners_dataset'
+    BQ_TABLE_NAME = 'gsc_daily_table'
+
+
+    # if testing locally
+    from unittest.mock import Mock
+    data = {
+        'site': site, 
+        'page_url':page_url, 
+        'start_date':start_date, 
+        'end_date':end_date, 
+        'BQ_DATASET_NAME':BQ_DATASET_NAME, 
+        'BQ_TABLE_NAME':BQ_TABLE_NAME
+    }
+    request = Mock(get_json=Mock(return_value=data), args=data)
+    run(request)
