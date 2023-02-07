4745a53ccbced638aba8c1838f2f5e40d8ac40fc
alton <alton@frontanalytics.com>
2023-02-07 15:37:03 -0700

    adding twitter

--- datasource-api-google-search-console/gsc_daily_cloud_function/test_main.py
+++ datasource-api-google-search-console/gsc_daily_cloud_function/test_main.py
@@ -8,7 +8,7 @@ def test_main():
 
 
     # construct input variables
-    site = "bikelanes.com"
+    site = "cupcaketree.com"
     BQ_DATASET_NAME = 'data_winners_dataset'
     BQ_TABLE_NAME = 'gsc_daily_table'
 
@@ -19,8 +19,8 @@ def test_main():
         'site': site, 
         'BQ_DATASET_NAME':BQ_DATASET_NAME, 
         'BQ_TABLE_NAME':BQ_TABLE_NAME,
-        'start_date': '2023-01-28',
-        'n_days_back':2*31
+        'start_date': '2023-01-27',
+        'n_days_back':2
     }
     request = Mock(get_json=Mock(return_value=data), args=data)
     run(request)
